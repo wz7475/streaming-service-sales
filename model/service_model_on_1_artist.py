2ef59521e5019b9f65109d078765808e187c3368
pgryz <01169171@pw.edu.pl>
2024-01-05 20:53:40 +0100

    Add building models

--- model/service_model_on_1_artist.py
+++ model/service_model_on_1_artist.py
@@ -1,15 +1,19 @@
 from math import sqrt
 from sklearn.metrics import mean_squared_error
-from config_file import data_path
+from model.config_file import data_path
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import os
 import warnings
+
 warnings.filterwarnings("ignore")
+
+
 def prepare_df(artist_filename: str) -> pd.DataFrame:
-    df = pd.read_csv(os.path.join(data_path, 'artists_sessions', artist_filename))
+    df = pd.read_csv(
+        os.path.join(data_path, 'artists_sessions', artist_filename))
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     df = df.set_index("timestamp")
     df.drop(["id_artist"], axis=1, inplace=True)
@@ -24,13 +28,16 @@ def get_trained_model(train) -> SARIMAX:
     sarima_fit = sarima.fit(disp=False)
     return sarima_fit
 
+
 def get_dummy_prediction(train, periods: int):
     dummy_pred = np.zeros(periods)
     for i in range(periods):
         if i == 0:
             dummy_pred[i] = train[- (periods) + i:].mean()
         else:
-            dummy_pred[i] = train[- (periods) + i:].mean() * (periods - i) / periods + dummy_pred[:i].mean() * i / periods
+            dummy_pred[i] = train[- (periods) + i:].mean() * (
+                        periods - i) / periods + dummy_pred[
+                                                 :i].mean() * i / periods
     return dummy_pred
 
 
@@ -39,7 +46,8 @@ def check_artist(artist_id: str) -> (float, float):
     train = df_resampled['count'].values[:len(df_resampled) - 8]
     test = df_resampled['count'].values[len(df_resampled) - 8:]
     sarima_fit = get_trained_model(train)
-    pred = sarima_fit.predict(start=len(train), end=len(train) + len(test) - 1, dynamic=False)
+    pred = sarima_fit.predict(start=len(train), end=len(train) + len(test) - 1,
+                              dynamic=False)
     dummy_pred = get_dummy_prediction(train, len(test))
 
     sarima_rmse = sqrt(mean_squared_error(test, pred))
@@ -48,12 +56,54 @@ def check_artist(artist_id: str) -> (float, float):
     return sarima_rmse / sum_of_test, dummy_rmse / sum_of_test
 
 
+class NaiveModel:
+    def __init__(self, train):
+        self.train = [t for t in train]
+
+    def predict(self, start, end: int, **kwargs):
+        copy = [t for t in self.train]
+
+        for period in range(len(self.train), max(len(self.train), end)):
+            copy.append(
+                sum(copy[-4 + period:]) / 4
+            )
+
+        return copy[start:end]
+
+
+def get_naive_model(train):
+    return NaiveModel(train)
+
+
+def generate_model(artist_id: str):
+    df_resampled = prepare_df(artist_id)
+    sarima_fit = get_trained_model(df_resampled['count'].values)
+    naive_fit = get_naive_model(df_resampled['count'].values)
+
+    return naive_fit, sarima_fit
+
+
+def generate_models():
+    all_artists = os.listdir(
+        os.path.join(os.getcwd(), data_path, "artists_sessions"))
+
+    for i, artist in enumerate(all_artists):
+        print(f'[{i} / {len(all_artists)}]{artist}')
+        naive, complex = generate_model(artist)
+        yield artist.replace('.csv', ''), naive, complex
+
+
 if __name__ == "__main__":
     all_artists = os.listdir(os.path.join(data_path, 'artists_sessions'))
 
-    df_score = pd.DataFrame(columns=['artist_id', 'sarima_score', 'dummy_score'])
-    for artist in all_artists:
+    df_score = pd.DataFrame(
+        columns=['artist_id', 'sarima_score', 'dummy_score'])
+    for i, artist in enumerate(all_artists):
+        print(f'[{i} / {len(all_artists)}]{artist}')
         sarima_score, dummy_score = check_artist(artist)
         print(sarima_score, dummy_score)
-        df_score = df_score._append({'artist_id': artist, 'sarima_score': sarima_score, 'dummy_score': dummy_score}, ignore_index=True)
-    df_score.to_csv(os.path.join(data_path, 'sarima_dummy_scores.csv'), index=False)
+        df_score = df_score._append(
+            {'artist_id': artist, 'sarima_score': sarima_score,
+             'dummy_score': dummy_score}, ignore_index=True)
+    df_score.to_csv(os.path.join(data_path, 'sarima_dummy_scores.csv'),
+                    index=False)
